dd8a44a44d2303b084e2db6abb789068b910cb1b
Ubuntu <gerard@idearobin.com>
2019-09-25 14:26:34 +0800

    ci: 🎡 modify for skywalk

--- .alfred/s3-deploy.sh
+++ .alfred/s3-deploy.sh
@@ -33,12 +33,3 @@ curl -X POST -s $SLACK_URL -d '{
 
 docker build -t $IMAGE_NAME -f Dockerfile.build .
 docker run --rm -e SKYWALK_API_HOST=/api -v $(pwd)/dist:/usr/src/dist $IMAGE_NAME
-
-aws s3 sync --acl public-read --sse --delete dist/ $S3_BUCKET
-
-# docker ps -a | grep -E Exited | awk -e '{print $1}' | xargs docker rm $GIT_REPO_NAME'-'$JOB_BASE_NAME
-# docker images | grep -E none | awk -e '{print $3}'| xargs docker rmi $GIT_REPO_NAME'-'$JOB_BASE_NAME
-curl -X POST -s $SLACK_URL -d '{
-  "type": "mrkdwn",
-  "text": "<'$BUILD_URL'console|'$BUILD_NUMBER'> *Synching* `'${COMMIT_SHA}'` to '$S3_BUCKET' *Complete*"
-}'
