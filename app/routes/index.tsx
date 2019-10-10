5adc80acd9eff212b7159330a1560e86d30fc12f
g-six <gerard@idearobin.com>
2019-10-10 12:18:25 +0800

    feat: 🎸 tech solutions

--- app/routes/index.tsx
+++ app/routes/index.tsx
@@ -4,6 +4,7 @@ import { Page404Error } from '@pages/404Error'
 import { HomePage } from '@pages/Home'
 import { OurTeam } from '@pages/Team'
 import { TalentSolutions } from '@pages/TalentSolutions'
+import { TechSolutions } from '@pages/TechnologySolutions'
 import * as React from 'react'
 import { Route, Switch, Redirect } from 'react-router'
 
@@ -14,6 +15,7 @@ const Routes = (
       <Route exact path="/" component={HomePage} />
       <Route exact path="/our-team" component={OurTeam} />
       <Route exact path="/talent-solutions" component={TalentSolutions} />
+      <Route exact path="/technology-solutions" component={TechSolutions} />
       <Route exact path="/four-oh-four" component={Page404Error} />
       <Redirect to="/four-oh-four" push={false} />
     </Switch>
